4d8a54b78ca9f4fa1c3c5701ebd0f366f83a6a6f
xavier-kong <kongxianchengxavier@gmail.com>
2021-08-18 16:50:59 +0800

    refactor into userRouter

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,11 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
-const User = require('./models/user')
-const Manga = require('./models/manga')
-const bcrypt = require('bcrypt')
 const loginRouter= require('./routers/loginRouter')
 const mangaRouter= require('./routers/mangaRouter')
 const usersRouter= require('./routers/usersRouter')
+const userRouter= require('./routers/userRouter')
 
 require('dotenv').config()
 
@@ -31,40 +28,10 @@ const getTokenFrom = request => {
   return null
 }
 
-app.get('/api/user/mangas', async (req, res) => {
-  const token = getTokenFrom(req) // refactor into own function?
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
-    return res.status(401).json({ error: 'token missing or invalid' })
-  }
-  const user = await User.findById(decodedToken.id).populate('mangas.manga')
-  res.json(user)
-})
-
-app.put('/api/user', async (req,res) => {
-  const token = getTokenFrom(req)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
-    return res.status(401).json({ error: 'token missing or invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
-  const mangaIndex = await user.mangas.findIndex(manga => String(manga._id) === String(req.body.id))
-  if (req.body.status) {
-    user.mangas[mangaIndex].status = req.body.status
-  }
-  if (req.body.chapter) {
-    user.mangas[mangaIndex].chapter = req.body.chapter
-  }
-  if (req.body.lastRead) {
-    user.mangas[mangaIndex].lastRead = req.body.lastRead
-  }
-  const updatedUser = await user.save()
-  res.json(updatedUser)
-})
-
 app.use('/api/manga', mangaRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/user', userRouter)
 
 const PORT = 3001
 
---
+++ backend/routers/userRouter.js
@@ -0,0 +1,45 @@
+const userRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+userRouter.get('/mangas', async (req, res) => {
+  const token = getTokenFrom(req) // refactor into own function?
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id).populate('mangas.manga')
+  res.json(user)
+})
+
+userRouter.put('/', async (req,res) => {
+  const token = getTokenFrom(req)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  const mangaIndex = await user.mangas.findIndex(manga => String(manga._id) === String(req.body.id))
+  if (req.body.status) {
+    user.mangas[mangaIndex].status = req.body.status
+  }
+  if (req.body.chapter) {
+    user.mangas[mangaIndex].chapter = req.body.chapter
+  }
+  if (req.body.lastRead) {
+    user.mangas[mangaIndex].lastRead = req.body.lastRead
+  }
+  await user.save()
+  const updatedUser = await User.findById(decodedToken.id).populate('mangas.manga')
+  res.json(updatedUser)
+})
+
+module.exports = userRouter
