c98409a7cef653f7488f4ff4c09d07a62df12004
xavier-kong <kongxianchengxavier@gmail.com>
2021-08-25 17:26:43 +0800

    manga update functionality

--- backend/routers/mangaRouter.js
+++ backend/routers/mangaRouter.js
@@ -29,4 +29,24 @@ mangaRouter.post('/', async (req, res) => {
   }
 })
 
+mangaRouter.put('/', async (req, res) => {
+  const user = req.user
+  const mangaIndex = await user.mangas.findIndex(manga => String(manga._id) === String(req.body._id))
+  if (req.body.status) {
+    user.mangas[mangaIndex].status = req.body.status
+  }
+  if (req.body.chapter) {
+    user.mangas[mangaIndex].chapter = req.body.chapter
+  }
+  if (req.body.lastRead) {
+    user.mangas[mangaIndex].lastRead = req.body.lastRead
+  }
+  if (req.body.link) {
+    user.mangas[mangaIndex].link = req.body.link
+  }
+  await user.save()
+  const updatedUser = await User.findById(req.user._id)
+  res.json(updatedUser.mangas[mangaIndex])
+})
+
 module.exports = mangaRouter
